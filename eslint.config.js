c9b9262e2a378ed01fad56344ac987ac03dd5c6e
Matias Amuchástegui <matias@condamind.com>
2025-08-05 17:46:12 -0300

    linter config

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettier,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['eslint.config.js', '.eslintrc.js', 'dist/**', 'node_modules/**'],
+  },
+];
